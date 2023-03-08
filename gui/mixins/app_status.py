305093117dff310907f6a7a7751af3485f095380
Zach Domke <zdomke@slac.stanford.edu>
2023-03-07 16:58:43 -0800

    Complete first version of App Status

--- gui/mixins/app_status.py
+++ gui/mixins/app_status.py
@@ -1,10 +1,11 @@
 from functools import partial
 from epics import PV
 from epics.dbr import DBE_VALUE
-from qtpy.QtCore import Qt
+from qtpy.QtCore import (Qt, Slot)
 from qtpy.QtWidgets import QHeaderView
 from mps_database.models import ApplicationCard
-from models_pkg.app_status_model import AppStatusTable
+from models_pkg.logic_model import MPSSortFilterModel
+from models_pkg.app_status_model import (AppStatusTable, RelatedDisplayDelegate)
 
 
 class AppStatusMixin:
@@ -12,23 +13,46 @@ class AppStatusMixin:
         """Initializer for the App Status tab."""
         self.apps = self.model.config.session.query(ApplicationCard).all()
 
-        self.app_model = AppStatusTable(self, self.model.config.Session, self.apps)
+        self.app_tbl_model = AppStatusTable(self, self.model.config.Session, self.apps)
+        self.rd_button_delegate = RelatedDisplayDelegate(self.ui.app_status_tbl)
 
-        # TODO: Sorting does nothing yet. Implement SortFilterProxyModel
+        self.app_model = MPSSortFilterModel(self)
+        self.app_model.setSourceModel(self.app_tbl_model)
+
         self.ui.app_status_tbl.setModel(self.app_model)
         self.ui.app_status_tbl.sortByColumn(5, Qt.AscendingOrder)
 
+        self.ui.app_status_tbl.setItemDelegate(self.delegate)
+        self.ui.app_status_tbl.setItemDelegateForColumn(6, self.rd_button_delegate)
+        for row in range(len(self.apps)):
+            self.ui.app_status_tbl.openPersistentEditor(self.app_model.index(row, 6))
+
         hdr = self.ui.app_status_tbl.horizontalHeader()
-        hdr.setSectionResizeMode(QHeaderView.Stretch)
+        hdr.setSectionResizeMode(5, QHeaderView.Stretch)
+        hdr.resizeSection(6, 100)
 
         self.app_pvs = []
 
     def app_status_connections(self):
+        """Establish App Status connections with PVs and Signals."""
         for i, app in enumerate(self.apps):
             app_pv = PV(f"{app.link_node.get_cn_prefix()}:APP{app.number}_STATUS",
                         callback=partial(self.send_app_status, row=i),
                         auto_monitor=DBE_VALUE)
             self.app_pvs.append(app_pv)
 
+            # Establish connections for showing the row count
+            self.app_model.rowsRemoved.connect(self.show_app_row_count)
+            self.app_model.rowsInserted.connect(self.show_app_row_count)
+            self.app_model.layoutChanged.connect(self.show_app_row_count)
+
     def send_app_status(self, value: int, row: int, **kw):
-        self.app_model.status_signal.emit(value, row)
+        """Function to emit the status signal in the model."""
+        self.app_tbl_model.status_signal.emit(value, row)
+
+    @Slot()
+    def show_app_row_count(self):
+        """When the number of displayed rows changes, update the row
+        count at the bottom of the tab."""
+        rows = self.app_model.rowCount()
+        self.ui.num_apps_lbl.setText(f"Displaying {rows} / {len(self.apps)} Faults")
--- gui/models_pkg/app_status_model.py
+++ gui/models_pkg/app_status_model.py
@@ -1,10 +1,12 @@
 from qtpy.QtCore import (Qt, Slot, Signal, QModelIndex, QAbstractTableModel)
+from qtpy.QtWidgets import QStyledItemDelegate
 from sqlalchemy.orm import (sessionmaker, scoped_session)
+from pydm.widgets import PyDMRelatedDisplayButton
 from enums import Statuses
 
 
 class AppStatusTable(QAbstractTableModel):
-    hdr_lst = ["LN", "Loc", "Slot", "AID", "Type", "Status"]
+    hdr_lst = ["LN", "Loc", "Slot", "AID", "Type", "Status", "Group"]
 
     status_signal = Signal(int, int)
 
@@ -14,6 +16,8 @@ class AppStatusTable(QAbstractTableModel):
         self.apps = apps
 
         self._data = []
+        self.status = []
+        self.channels = []
         self.set_data()
 
         self.status_signal.connect(self.set_status)
@@ -37,15 +41,10 @@ class AppStatusTable(QAbstractTableModel):
         elif role == Qt.BackgroundRole:
             return Statuses.BGD.brush()
         elif role == Qt.ForegroundRole:
+            row = index.row()
             col = index.column()
-            txt = index.data()
             if col == 5:
-                if txt == "ONLINE":
-                    return Statuses.GRN.brush()
-                elif txt == "OFFLINE":
-                    return Statuses.RED.brush()
-                else:
-                    return Statuses.WHT.brush()
+                return self.status[row].brush()
             else:
                 return Statuses.GRN.brush()
 
@@ -58,17 +57,75 @@ class AppStatusTable(QAbstractTableModel):
     def set_data(self):
         """Set initial data for every app."""
         for app in self.apps:
-            lst = [app.name] * len(self.hdr_lst)
+            ch = f"{app.link_node.get_cn_prefix()}:APP{app.number}_STATUS"
+
+            lst = [ch] * len(self.hdr_lst)
             lst[0] = app.link_node.lcls1_id
             lst[1] = app.crate.location
             lst[2] = app.slot_number if app.slot_number != 1 else "RTM"
             lst[3] = app.number
             lst[4] = app.type.name
+            lst[6] = app.link_node.group
 
             self._data.append(lst)
+            self.status.append(Statuses.WHT)
+            self.channels.append(ch)
 
     @Slot(int, int)
     def set_status(self, value: int, row: int):
         """Set the App's Status based on the value passed."""
         self._data[row][5] = "ONLINE" if value else "OFFLINE"
+        self.status[row] = Statuses.GRN if value else Statuses.RED
         self.dataChanged.emit(self.index(row, 5), self.index(row, 5))
+
+    def less_than(self, left: QModelIndex, right: QModelIndex):
+        """Called by MPSSortFilterProxyModel to sort rows based on the
+        app's status."""
+        if left.column() in [0, 2, 3]:
+            left_state = int(left.data().replace("RTM", "1"))
+            right_state = int(right.data().replace("RTM", "1"))
+        elif left.column() == 5:
+            left_state = self.status[left.row()].num()
+            if self.status[left.row()] == Statuses.WHT:
+                left_state = Statuses.max() + 1
+            right_state = self.status[right.row()].num()
+            if self.status[right.row()] == Statuses.WHT:
+                right_state = Statuses.max() + 1
+        else:
+            left_state = left.data()
+            right_state = right.data()
+        return left_state < right_state
+
+    def filter_accepts_row(self, row: int, parent: QModelIndex, filters: dict):
+        """Called by MPSSortFilterProxyModel to filter out rows based on
+        the table's needs."""
+        for col, text in filters.items():
+            if col == 5:
+                if not self.status[row].faulted():
+                    return False
+            else:
+                ind = self.index(row, col, parent)
+                if ind.isValid() and (text not in str(ind.data()).lower()):
+                    return False
+        return True
+
+    def middle_click_data(self, index: QModelIndex):
+        """Returns the text to be copied to the clipboard."""
+        return self.channels[index.row()]
+
+
+# TODO: Generalize this
+class RelatedDisplayDelegate(QStyledItemDelegate):
+    """Customized QStyledItemDelegate to allow the user to open an
+    associated display."""
+    def __init__(self, parent):
+        super(RelatedDisplayDelegate, self).__init__(parent)
+
+    def createEditor(self, parent, option, index):
+        """Create the RelatedDisplayButton to show for the index."""
+        file = f"$PHYSICS_TOP/mps_configuration/current/display/groups/LinkNodeGroup{index.data()}.ui"
+        editor = PyDMRelatedDisplayButton(parent=parent, filename=file)
+        editor.setText(f"Group {index.data()}...")
+        editor.showIcon = False
+        editor.openInNewWindow = True
+        return editor
